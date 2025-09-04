150a9d5647cb113df9aa634872aa8f80d1d016e4
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2025-09-04 18:45:50 +0300

    Added AIME2025 dataset

---
+++ tsllm/envs/aime2024/env.py
@@ -0,0 +1,93 @@
+import copy
+import re
+from typing import List, Optional
+import numpy as np
+from tsllm.envs.base_env import CoTEnv, NoLegalActionException, INVALID_ANS
+from .prompt import COT_EXAMPLES, COT_TASK_DESC, PROBLEM_FORMAT_STR, SEP
+
+ANS_RE = re.compile(r"The answer is (\-?[0-9\.\,]+)")
+STOP_STR = "The answer is "
+QUESTION_KEY = "problem"
+
+
+def extract_answer(completion):
+    match = ANS_RE.search(completion)
+    if match:
+        match_str = match.group(1).strip()
+        match_str = match_str.replace(",", "")
+    else:
+        return INVALID_ANS
+    return match_str
+
+
+def extract_groundtruth(groundtruth_str: str):
+    x = groundtruth_str.strip().replace(",", "")
+    try:
+        float(x)
+    except:
+        raise ValueError(
+            "Warning: Error should raise since the extracted groundtruth string {}\
+             cannot be converted to float".format(
+                x
+            )
+        )
+    return x
+
+
+def judge_correct(problem_str: str, extracted_groundtruth: Optional[str], answer: str):
+    float_groundtruth = float(extracted_groundtruth)
+    try:
+        return abs(float(answer) - float_groundtruth) < 1e-5
+    except Exception:
+        return False
+
+
+class Aime2024Env(CoTEnv):
+    sep = SEP
+
+    def __init__(
+        self,
+        config,
+        math_problems,
+        llm_gen_fn,
+        tokenizer,
+        task_desc_str: str = COT_TASK_DESC,
+        cot_example_str: str = COT_EXAMPLES,
+        problem_format_str: str = PROBLEM_FORMAT_STR,
+        reset=True,
+        action_distribution_temperature=1.0,
+    ):
+        super().__init__(
+            config,
+            math_problems,
+            llm_gen_fn,
+            tokenizer,
+            task_desc_str,
+            cot_example_str,
+            problem_format_str,
+            reset,
+            action_distribution_temperature,
+        )
+
+    @property
+    def stop_str(self):
+        return STOP_STR
+
+    def _is_correct(self, completion):
+        extracted_answer = extract_answer(completion)
+        # print("Compare: {} -- {}".format(extrated_answer,
+        #  self.math_problem['answer']))
+        # return extrated_answer == self.math_problem['answer']
+        return judge_correct(
+            self.math_problem['question'], self.math_problem["answer"], extracted_answer
+        )
+
+    def init_action_history(self):
+        # add the first prompted questions
+        return ([self.task_prefix] if self.task_prefix is not None else []) + [
+            f"Question: {self.math_problem['question']}\nAnswer: Let's think step by step"
+        ]
+
+    def get_reward(self):
+        """To implement based on learned reward model"""
+        return 0
