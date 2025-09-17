dd557e5423ac87523d69568c03a5013c386f8c53
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2025-09-17 13:31:07 +0300

    Fixed error

--- tsllm/rl/trainer/mcts_trainer_traj_ct2_value_trl.py
+++ tsllm/rl/trainer/mcts_trainer_traj_ct2_value_trl.py
@@ -41,24 +41,24 @@ class AccelerateMCTSTrainer(BaseMCTSTrainer):
         self.model = self.setup_model()
         self.model = self.model.to(torch.bfloat16)
 
-        # peft_config = LoraConfig(
-        #     r=8,
-        #     lora_alpha=16,
-        #     target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
-        #     bias="none",
-        #     target_parameters=[
-        #         "7.mlp.experts.gate_up_proj",
-        #         "7.mlp.experts.down_proj",
-        #         "15.mlp.experts.gate_up_proj",
-        #         "15.mlp.experts.down_proj",
-        #         "23.mlp.experts.gate_up_proj",
-        #         "23.mlp.experts.down_proj",
-        #     ],
-        # )
-        # self.model = get_peft_model(self.model, peft_config)
-        # for name, param in self.model.named_parameters():
-        #     if 'v_head' in name:
-        #         param.requires_grad = True
+        peft_config = LoraConfig(
+            r=8,
+            lora_alpha=16,
+            target_modules=["q_proj", "k_proj", "v_proj", "o_proj"],
+            bias="none",
+            target_parameters=[
+                "7.mlp.experts.gate_up_proj",
+                "7.mlp.experts.down_proj",
+                "15.mlp.experts.gate_up_proj",
+                "15.mlp.experts.down_proj",
+                "23.mlp.experts.gate_up_proj",
+                "23.mlp.experts.down_proj",
+            ],
+        )
+        self.model = get_peft_model(self.model, peft_config)
+        for name, param in self.model.named_parameters():
+            if 'v_head' in name:
+                param.requires_grad = True
 
         self.opt = self.setup_optimizer()
 
